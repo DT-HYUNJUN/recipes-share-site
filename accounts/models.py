4155c61dd83ada9a72045ebe0ae86e67622e868c
pty9714 <pty9714@naver.com>
2023-05-23 11:10:43 +0900

    [Add] base_models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from recipes.models import Ingredient
 # Create your models here.
+
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=20, blank=True, null=False)
+    followers = models.ManyToManyField('self', related_name='followings', symmetrical=False)
+    profile_image = ProcessedImageField(upload_to='profile/', processors=[ResizeToFill(300, 300)], format='JPEG', options={'quality': 80}, null=True, blank=True)
+    fridge = models.ManyToManyField(Ingredient, related_name='fridge_users', blank=True)
--- communities/models.py
+++ communities/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_posts = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts_users', blank=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
