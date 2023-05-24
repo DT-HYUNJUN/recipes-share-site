547e60a1b5ee2ae3f89854d7c4704bffe02f5f13
Park <angle307@naver.com>
2023-05-24 17:51:20 +0900

    [ADD] recipes index templates

--- static/js/recipes/carousel.js
+++ static/js/recipes/carousel.js
@@ -1,9 +1,36 @@
-$(document).ready(function(){
-  $('.recipe-slide').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    prevArrow : "<button type='button' class='slick-prev slick-index-prev'>Previous</button>",    
-    nextArrow : "<button type='button' class='slick-next slick-index-next'>Next</button>",
-  });
-})
+// $(document).ready(function(){
+// })
+$('.recipe-slide').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow: '<button type="button" class="slick-prev"><</button>',
+  nextArrow: '<button type="button" class="slick-next">></button>',
+  // 이건 자동재생
+  // autoplay:true,
+  // autoplayspeed:10,
+  // 이건 자동재생때 멈추지 않는거 눌렀을때 포함
+  pauseOnHover: false,
+  pauseOnFocus: false,
+});
+
+
+
+// 아니 여기에 previous라고 적혀있네요 애초에 덜더
+
+
+
+// 차지하는 영역때매 그런걸까요 ? 
+
+// 음 zzzzzㅋㅋㅋㅋㅋㅋ 이상하게 되네여 ...
+// 아니 왜 버튼이 안 생겨요? 예제에는 그냥 된느거 처럼 나오는데
+// 저도 그래서 검색하다가 저렇게 넣으면 된다길래 해봤는데 이제 스윽 넘겨야 나오네여 ...오히려 좋아요  맞아요?
+//  자동넘김을 해볼까요 그럼 ? 한번 해보죠
+// 좋아요 새로고침하면 자동넘김 되요 !!!!!!!!!!!!!!!
+// 오 근데 버튼이 꼭 필요할까요?
+// ㅋㅋㅋㅋㅋㅋㅋ 조원들 설득해보죠 . 제 영혼없는 말투로 설득해보겠습니다
+// 오히려 조아
+// 근데 한번 만지니까 자동이 멈춰요
+// 만지면 멈추게 할까요 ? 그거 설정도 있는거 같아요 원래는 멈추나요?
+// 아니요 따로 설정하면 멈추나봐요 왜 저는 멈출까요....저도 다시 봐볼게요 누르면 멈추긴 하네요 .. 근데 또 다시 움직이는 .. 방금 제가 save해서 새로고침 된듯
+// 오 그런일이 듯 갑자기 다 멈추는데 뭘까요 ? 안넘어가요? 이제 되용 ㅋㅋㅋ  버튼이 그리우신가요 ? 걸렸나요 저두 알아볼게용 
