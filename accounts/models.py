39549a51af5dda65688ef57e6f8b51c913827dd4
Park <angle307@naver.com>
2023-05-26 10:44:00 +0900

    [ADD] base index template

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from recipes.models import Ingredient
 
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=20, blank=True, null=False)
+    nickname = models.CharField(max_length=8, blank=True, null=False)
     followers = models.ManyToManyField('self', related_name='followings', symmetrical=False)
     profile_image = ProcessedImageField(upload_to='profile/', processors=[ResizeToFill(300, 300)], format='JPEG', options={'quality': 80}, null=True, blank=True)
     fridge = models.ManyToManyField(Ingredient, related_name='fridge_users', blank=True)
