308426f35790fd6bacc29b19d49d50081cfa6eed
Park <angle307@naver.com>
2023-05-27 00:28:55 +0900

    [ADD] recipes detail review

--- static/js/recipes/review.js
+++ static/js/recipes/review.js
@@ -2,72 +2,73 @@ const reviewSection = document.querySelector('#reviews')
 const reviews = document.querySelectorAll('#review')
 
 reviews.forEach(review => {
-  const input = review.querySelector('input')
+  const commentTextarea = review.querySelector('#comment-textarea')
+  const input = review.querySelector('textarea')
   const aTags = review.querySelectorAll('a')
   const editBtn = aTags[0]
   const deleteBtn = aTags[1]
-  const submitSection = review.querySelector('#submit-section')
-  const submitBtn = submitSection.querySelectorAll('span')[0]
-  const cancelBtn = submitSection.querySelectorAll('span')[1]
+  const submitBtn = review.querySelector('#submit-btn')
+  const cancelBtn = review.querySelector('#cancel-btn')
   const reviewContent = review.querySelector('#review-content')
-  
-  editBtn.addEventListener('click', () => {
-    reviewContent.classList.add('hidden')
-    input.classList.remove('hidden')
-    submitSection.classList.remove('invisible')
-  })
 
-  cancelBtn.addEventListener('click', () => {
-    reviewContent.classList.remove('hidden')
-    input.classList.add('hidden')
-    submitSection.classList.add('invisible')
-  })
+  if (editBtn && deleteBtn) {
+    editBtn.addEventListener('click', () => {
+      reviewContent.classList.add('hidden')
+      commentTextarea.classList.remove('hidden')
 
-  submitBtn.addEventListener('click', () => {
-    let param = {
-      'reviewPk': review.dataset.reviewPk,
-      'content': input.value
-    }
-
-    $.ajax({
-      url: review.dataset.updateUrl,
-      type: 'POST',
-      headers: {
-        'X-CSRFTOKEN': review.dataset.csrftoken
-      },
-      data: JSON.stringify(param),
-      success:function(data) {
-        input.value = data.content
-        input.readOnly = true
-        input.classList.remove('border-1')
-        input.classList.add('border-0')
-        submitSection.classList.add('invisible')
-      },
-      error: function() {
-        alert('오류!')
-      }
     })
-  })
+  
+    cancelBtn.addEventListener('click', () => {
+      reviewContent.classList.remove('hidden')
+      commentTextarea.classList.add('hidden')
+
+    })
+  
+    submitBtn.addEventListener('click', () => {
+      let param = {
+        'pk': review.dataset.reviewPk,
+        'content': input.value
+      }
+      
+      $.ajax({
+        url: review.dataset.updateUrl,
+        type: 'POST',
+        headers: {
+          'X-CSRFTOKEN': review.dataset.csrftoken
+        },
+        data: JSON.stringify(param),
+        success:function(data) {
+          reviewContent.textContent = data.content
+          commentTextarea.classList.add('hidden')
+          reviewContent.classList.remove('hidden')
+        },
+        error: function() {
+          alert('오류!')
+        }
+      })
+    })
+  }
+  
 
   // deleteBtn.addEventListener('click', () => {
-  //   // let param = {
-  //   //   'reviewPk': review.dataset.reviewPk,
-  //   //   'recipePk': review.dataset.recipePk,
-  //   // }
+  //   let param = {
+  //     'reviewPk': review.dataset.reviewPk,
+  //     'recipePk': review.dataset.recipePk,
+  //   }
 
-  //   // $.ajax({
-  //   //   url: review.dataset.deleteUrl,
-  //   //   type: 'POST',
-  //   //   headers: {
-  //   //     'X-CSRFTOKEN': review.dataset.csrftoken
-  //   //   },
-  //   //   data: JSON.stringify(param),
-  //   //   success:function(data) {
-  //   //     reviewSection.removeChild(review)
-  //   //   },
-  //   //   error: function() {
-  //   //     alert('오류!')
-  //   //   }
-  //   // })
+  //   $.ajax({
+  //     url: review.dataset.deleteUrl,
+  //     type: 'POST',
+  //     headers: {
+  //       'X-CSRFTOKEN': review.dataset.csrftoken
+  //     },
+  //     data: JSON.stringify(param),
+  //     success:function(data) {
+  //       reviewSection.removeChild(review)
+  //     },
+  //     error: function() {
+  //       alert('오류!')
+  //     }
+  //   })
   // })
 });
