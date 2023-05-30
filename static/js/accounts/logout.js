31b8e107cf68efcb1e921dda37ab8f12d44ad7a7
Park <angle307@naver.com>
2023-05-30 17:44:42 +0900

    [UPDATE] logout modal

---
+++ static/js/accounts/logout.js
@@ -0,0 +1,9 @@
+// const logoutModal = document.getElementById('popup-modal')
+
+// if (logoutModal.isVisible) {
+//   console.log('로그아웃')
+// }
+
+setTimeout(function () {
+  hideModal();
+},7000);
