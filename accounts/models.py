7721d72c2f8447f9e62f32b31341085a4f7898f6
lemon-lime-honey <bs0308@g.skku.edu>
2023-05-30 10:20:09 +0900

    [UPDATE] requirements for signup

--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 from recipes.models import Ingredient
-# Create your models here.
 
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=8, blank=True, null=False)
+    email = models.EmailField('이메일', blank=False)
+    nickname = models.CharField('닉네임', max_length=8, null=False)
     followers = models.ManyToManyField('self', related_name='followings', symmetrical=False)
     profile_image = ProcessedImageField(upload_to='profile/', processors=[ResizeToFill(300, 300)], format='JPEG', options={'quality': 80}, null=True, blank=True)
     fridge = models.ManyToManyField(Ingredient, related_name='fridge_users', blank=True)
