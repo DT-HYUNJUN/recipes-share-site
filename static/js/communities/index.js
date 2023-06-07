eb3aa495a8346e8610f4a0b895b588b537dcdf2c
Park <angle307@naver.com>
2023-06-07 09:57:23 +0900

    [ADD] kakaotalk share

--- static/js/communities/index.js
+++ static/js/communities/index.js
@@ -3,33 +3,55 @@ const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
 
 forms.forEach( (form) => {
   form.addEventListener('submit', function(event) {
-    event.preventDefault()
+  //   event.preventDefault()
 
-    const postId = event.target.dataset.postId
-    axios({
-      method : 'post',
-      url: `http://127.0.0.1:8000/communities/${postId}/like/`,
-      headers : {'X-CSRFToken': csrftoken},
-    })
+  //   const postId = event.target.dataset.postId
+  //   axios({
+  //     method : 'post',
+  //     url: `http://127.0.0.1:8000/communities/${postId}/like/`,
+  //     headers : {'X-CSRFToken': csrftoken},
+  //   })
 
-    .then((response) => {
-      const like = response.data.like
-      // const likeBtn = document.querySelector(`#like-${postId}`)
-      const likeBtni = form.querySelector('#like-heart')
-      const postLikeCount = form.querySelector('#post-like-count')
-      if (like === true) {
-        postLikeCount.textContent = parseInt(postLikeCount.textContent) + 1
-        likeBtni.classList.remove('bi-heart')
-        likeBtni.classList.add('bi-heart-fill')
-      } else {
-        postLikeCount.textContent = parseInt(postLikeCount.textContent) - 1
-        likeBtni.classList.remove('bi-heart-fill')
-        likeBtni.classList.add('bi-heart')
-      }
-    })
+  //   .then((response) => {
+  //     const like = response.data.like
+  //     // const likeBtn = document.querySelector(`#like-${postId}`)
+  //     const likeBtni = form.querySelector('#like-heart')
+  //     const postLikeCount = form.querySelector('#post-like-count')
+  //     if (like === true) {
+  //       postLikeCount.textContent = parseInt(postLikeCount.textContent) + 1
+  //       likeBtni.classList.remove('bi-heart')
+  //       likeBtni.classList.add('bi-heart-fill')
+  //     } else {
+  //       postLikeCount.textContent = parseInt(postLikeCount.textContent) - 1
+  //       likeBtni.classList.remove('bi-heart-fill')
+  //       likeBtni.classList.add('bi-heart')
+  //     }
+  //   })
 
-    .catch((error) => {
-      console.log(error.response)
-    })
+  //   .catch((error) => {
+  //     console.log(error.response)
+  //   })
+  event.preventDefault()
+  const postId = event.target.dataset.postId
+  let likePostCount = form.querySelector('#post-like-count')
+
+  axios({
+    method : 'post',
+    url: `/communities/${postId}/like/`,
+    headers: {'X-CSRFToken': csrftoken},
+  })
+  .then((response) => {
+    const isLiked = response.data.is_liked
+    const likeIcon = form.querySelector('i')
+    if (isLiked === true) {
+      likeIcon.classList.remove('bi-heart')
+      likeIcon.classList.add('bi-heart-fill')
+      likePostCount.textContent = parseInt(likePostCount.textContent) + 1
+    } else {
+      likeIcon.classList.add('bi-heart')
+      likeIcon.classList.remove('bi-heart-fill')
+      likePostCount.textContent = parseInt(likePostCount.textContent) - 1
+    }
+  })
   })
 })
---
+++ static/js/recipes/kakao-share.js
@@ -0,0 +1,60 @@
+const kakaoBtn = document.getElementById('btnKakao')
+const title = kakaoBtn.dataset.title
+const content = kakaoBtn.dataset.content
+const baseLink = 'http://127.0.0.1:8000'
+const link = kakaoBtn.dataset.link
+const image = kakaoBtn.dataset.image
+const toastSimple = document.getElementById('toast-simple')
+
+
+function shareKakao() {
+  // 사용할 앱의 JavaScript 키 설정
+  Kakao.init('46fed7723052d8712b267e8a83ab2fa4');
+  // 카카오링크 버튼 생성
+  Kakao.Link.createDefaultButton({
+    container: '#btnKakao', // 카카오공유버튼ID
+    objectType: 'feed',
+    content: {
+      title: title, // 보여질 제목
+      description: content, // 보여질 설명
+      imageUrl: 'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg', // 콘텐츠 URL
+      link: {
+         mobileWebUrl: baseLink + link,
+         webUrl: baseLink + link
+      }
+    }
+  });
+}
+
+
+function showToast() {
+  toastSimple.classList.remove('hidden');
+  toastSimple.classList.add('flex');
+
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const toastTop = scrollTop - 200; // 토스트 메시지의 상단 위치 조정
+  const viewportWidth = window.innerWidth;
+  const toastWidth = toastSimple.offsetWidth;
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  const currentLeft = scrollLeft + (viewportWidth / 2) - (toastWidth / 2);
+
+  toastSimple.style.top = toastTop + 'px';
+  toastSimple.style.left = currentLeft + 'px';
+  
+
+  setTimeout(() => {
+    toastSimple.classList.remove('flex');
+    toastSimple.classList.add('hidden');
+  }, 3000);
+}
+
+function copyLink() {
+  var linkElement = document.createElement('textarea');
+  linkElement.value = window.location.href;
+  document.body.appendChild(linkElement);
+  linkElement.select();
+  document.execCommand('copy');
+  document.body.removeChild(linkElement);
+  
+  showToast()
+}
