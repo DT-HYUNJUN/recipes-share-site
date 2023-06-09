c56af5d85e794361926fc4cfdb2f4fbfa6a348a4
DT-HYUNJUN <angle307@naver.com>
2023-06-09 17:07:08 +0900

    [UPDATE] settings.py
    [UPDATE] settings.py

--- pjt/settings.py
+++ pjt/settings.py
@@ -39,7 +39,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'port-0-recipes-share-site-17xqnr2algn95l6h.sel3.cloudtype.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
